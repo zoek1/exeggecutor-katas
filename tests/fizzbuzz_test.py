c37daadf4f0501649b10ae3b9bf1fdf4690d1c60
Miguel Angel Gordian <os.aioria@gmail.com>
2017-01-21 22:24:20 -0600

    Non mutiples of three remains equals

--- tests/fizzbuzz_test.py
+++ tests/fizzbuzz_test.py
@@ -6,3 +6,7 @@ class TestFizzBuzz(TestCase):
       multiples = [3, 6, 9, 12]
       fizz_list = ['Fizz'] * len(multiples) 
       self.assertEqual(fizzbuzz(multiples), fizz_list, "All multiples of three must be subtituted by 'Fizz'")
+
+  def test_non_multiples_of_three_remains_equals(self):
+      non_multiples = [5, 7, 8, 11, 17, 13, 19]
+      self.assertEqual(fizzbuzz(non_multiples), non_multiples, "Non multiples of three must remains equals")
