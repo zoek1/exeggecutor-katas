c694e85a897d20bfe800792a0c96fffb1ee28713
Miguel Angel Gordian <os.aioria@gmail.com>
2017-01-21 22:51:36 -0600

    refactor: fizz method now translate multiples of 3 to 'Fizz'

--- katas/fizzbuzz.py
+++ katas/fizzbuzz.py
@@ -1,6 +1,8 @@
 from future.utils import lmap
 
-def fizzbuzz(input):
-    multiples_3 = lmap(lambda x: (x % 3 == 0 and "Fizz") or x, input)
+def fizz(input):
+    return lmap(lambda x: (x % 3 == 0 and "Fizz") or x, input) 
 
-    return multiples_3
+
+def fizzbuzz(input):
+    pass
--- tests/fizzbuzz_test.py
+++ tests/fizzbuzz_test.py
@@ -1,12 +1,12 @@
-from unittest import TestCase
-from katas.fizzbuzz import fizzbuzz
+import unittest
+from katas.fizzbuzz import fizz
 
-class TestFizzBuzz(TestCase):
+class TestFizz(unittest.TestCase):
   def test_if_fizz_is_substituted_instead_of_multiples_of_three(self):
       multiples = [3, 6, 9, 12]
       fizz_list = ['Fizz'] * len(multiples) 
-      self.assertEqual(fizzbuzz(multiples), fizz_list, "All multiples of three must be subtituted by 'Fizz'")
+      self.assertEqual(fizz(multiples), fizz_list, "All multiples of three must be subtituted by 'Fizz'")
 
   def test_non_multiples_of_three_remains_equals(self):
-      non_multiples = [5, 7, 8, 11, 17, 13, 19]
-      self.assertEqual(fizzbuzz(non_multiples), non_multiples, "Non multiples of three must remains equals")
+      non_multiples = [7, 8, 11, 17, 13, 19]
+      self.assertEqual(fizz(non_multiples), non_multiples, "Non multiples of three must remains equals")
